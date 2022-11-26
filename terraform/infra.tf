e67b97250364fcadec0be67ad4c7c73e63f843d7
abdellatifone <abdellatifone7@gmail.com>
2022-11-26 22:40:16 +0100

    Update infra.tf

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -22,3 +22,42 @@ resource "google_storage_bucket" "static-site" {
   force_destroy = true
   uniform_bucket_level_access = true
 }
+
+
+# this is your Terraform Snowflake provider
+terraform {
+    required_providers {
+        snowflake = {
+            source  = "Snowflake-Labs/snowflake"
+            version = "0.39.0"
+        }
+    }
+}
+# connect to your Snowflake account
+provider "snowflake" {
+    account = "BOXPYLA.HI68725"
+    username = "ASKALLI"
+    password = "A6D9BAA5b15a$"
+    role = "accountadmin"
+}
+# create one "my_tf_database.my_tf_schema.my_tf_table" db table
+resource "snowflake_database" "database" {
+  name      = "my_tf_database"
+}
+resource "snowflake_schema" "schema" {
+  database  = snowflake_database.database.name
+  name      = "my_tf_schema"
+}
+resource "snowflake_table" "table" {
+  database  = snowflake_database.database.name
+  schema    = snowflake_schema.schema.name
+  name      = "my_tf_table"
+column {
+    name     = "id"
+    type     = "int"
+  }
+  column {
+    name     = "data"
+    type     = "text"
+  }
+}
