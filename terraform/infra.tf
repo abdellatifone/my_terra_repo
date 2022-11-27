3feb204d8cd52d2dff43c03ebbaff1353aa91ee0
abdellatifone <abdellatifone7@gmail.com>
2022-11-27 12:54:47 +0100

    Update infra.tf

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -15,6 +15,12 @@ resource "google_storage_bucket" "funky-bucket" {
   location  = "us-central1"
 }
 
+resource "google_storage_bucket" "static-site" {
+  name          = "image-store-com13"
+  location      = "EU"
+  force_destroy = true
+  uniform_bucket_level_access = true
+}
 
 resource "google_storage_bucket" "static-site" {
   name          = "image-store-com133"
